504ff6742ce3f56722d48016798ac4e1aa056054
Anushka Bora <anumbora2@gmail.com>
2026-04-15 19:08:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // ✅ VERY IMPORTANT FIX
+  base: "/",   // important
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 
   server: {
     proxy: {
